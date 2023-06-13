bbe8e9d4e2486e09cc7ca89806023965c1f6fe72
0xZales <rozalesamiral@gmail.com>
2023-06-13 10:48:30 +0000

    fix data to data.transactions

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,14 +4,15 @@ import { db } from '@yaris/lib/db';
 async function getTransaction() {
     const transactions = await db.transaction.findMany()
     return transactions
-  }
+}
 export default async function Dashboard({ }) {
     const data = await getData()
+    console.log(data)
     // const data = await getTransaction()
     return (
         <div className=' bg-muted rounded-lg m-5 flex flex-col gap-8 p-12'>
             <h1 className='font-heading text-2xl'>Transactions</h1>
-            <DataTable transactions={data} />
+            <DataTable transactions={data.transactions} />
             {/* <DataTable /> */}
         </div>
     );
