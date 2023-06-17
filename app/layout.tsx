a5fb0bb107af04e62ed1beca2b6b94030aad1cfe
0xZales <rozalesamiral@gmail.com>
2023-06-17 09:40:25 +0000

    update og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata = {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [`${siteConfig.url}/og.jpg`],
+    images: [`${siteConfig.url}/og.png`],
     creator: "@0xZales",
   },
   icons: {
