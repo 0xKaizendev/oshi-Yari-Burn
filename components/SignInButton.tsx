2ccc6625c592fac7ef0f53f137f6dfa844091ee9
0xZales <rozalesamiral@gmail.com>
2023-06-19 13:12:26 +0000

    fix login button color

--- components/SignInButton.tsx
+++ components/SignInButton.tsx
@@ -12,7 +12,7 @@ export default function SignInButton ({  })  {
   return (
     <div className='bg-gradient-to-r  from-green-600 to-green-800 p-2 rounded-xl'>
     {currentAccount !== null ? `${currentAccount.slice(0, 7)}...${currentAccount.slice(35)}` : (<Button className={` relative bg-gradient-to-r  from-green-600 to-green-800 md:text-sm text-xs h-8 px-2 md:h-10 md:py-2 md:px-4`} onClick={connectWallet}>
-      <span className={`${isLoading ? "text-blue-600 hover:text-blue-700" : null}`}> Connect Wallet</span>
+      <span className='text-primary hover:text-primary/90'> Connect Wallet</span>
       {isLoading ? <LucideLoader2 className='animate-spin absolute' /> : null}
     </Button>)}
   </div>
