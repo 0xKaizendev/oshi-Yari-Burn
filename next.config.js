5d4a54f4ac799eac69a10d811510ba90e0fa4d64
0xZales <rozalesamiral@gmail.com>
2023-07-09 17:14:32 -0700

    allow image from every source

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'tailwindui.com'
-            }
-        ]
-    },   experimental: {
-    appDir: true,
-  },
+    images:{
+        remotePatterns:[{
+            protocol:'https',
+            hostname:'**'
+        }]
+    }
 }
 
 module.exports = nextConfig
